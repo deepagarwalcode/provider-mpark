38684d981ce4964b13de840aa75d189ec26a9368
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-09-19 11:21:04 +0530

    User SDK

---
+++ src/lib/api/user.js
@@ -0,0 +1,12 @@
+class User {
+  axios;
+
+  constructor(axios) {
+    this.axios = axios;
+  }
+
+  async getMe() {
+    const { data } = await this.axios.get("/user/me");
+    return data;
+  }
+}
