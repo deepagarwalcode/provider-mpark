9eb23bc0d8d9717ae3a617094629405ae0759c90
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-09-19 11:20:55 +0530

    Parking SDK

---
+++ src/lib/api/parking.js
@@ -0,0 +1,17 @@
+class Parking {
+  axios;
+
+  constructor(axios) {
+    this.axios = axios;
+  }
+
+  async createParking(input) {
+    const { data } = this.axios.post("/parking", input);
+    return data;
+  }
+
+  async getMyParkings() {
+    const { data } = this.axios.get("/parking");
+    return data;
+  }
+}
